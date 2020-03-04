a209558e3867244694808e78a0f20068e1a54ced
wear99 <finne.sun@qq.com>
2020-03-04 21:05:04 +0800

    1

--- IO编程/文件读写.py
+++ IO编程/文件读写.py
@@ -3,7 +3,7 @@
 # 标示符'r'表示读，这样，我们就成功地打开了一个文件。如果文件不存在，open()函数就会抛出一个IOError的错误
 f = open(r'E:/test.txt', 'r', encoding='utf-8')
 # \ 在Python表示转义字符，不能直接使用：1.前面加r，表示直接引用；2.用 \\ ；3.用/代替
-# utf-8: 所有文件编码; gbk(ANSI),简体中文;  
+# utf-8: 所有文件编码; gbk(ANSI),简体中文;
 
 # 如果文件打开成功，接下来，调用read()方法可以一次读取文件的全部内容，Python把内容读到内存，用一个str对象表示：
 ss = f.read()
@@ -12,12 +12,11 @@ print(ss)
 # 最后一步是调用close()方法关闭文件
 f.close()
 
-
 # 由于文件读写时都有可能产生IOError，一旦出错，后面的f.close()就不会调用。
 # 所以，为了保证无论是否出错都能正确地关闭文件，我们可以使用try ... finally来实现：
 
 # Python引入了with语句来自动帮我们调用close()方法,简化代码
-with open('e:\\test.txt','r',encoding='utf-8') as ff:
+with open('e:\\test.txt', 'r', encoding='utf-8') as ff:
     print(ff.read())
 
 # 调用read()会一次性读取文件的全部内容;
@@ -25,9 +24,9 @@ with open('e:\\test.txt','r',encoding='utf-8') as ff:
 # 调用readlines()一次读取所有内容并按行返回list。
 # 调用read(size)方法，每次最多读取size个字节的内容
 
-with open(r'E:/test.txt','r',encoding='utf-8') as vsc:
+with open(r'E:/test.txt', 'r', encoding='utf-8') as vsc:
     for x in vsc.readlines():
-        print(x.strip())    #
+        print(x.strip())  #
 
 # 要读取二进制文件，比如图片、视频等等，用'rb'模式打开文件即可
 # 在文本文件中可能夹杂了一些非法编码的字符。遇到这种情况，open()函数还接收一个errors参数，
@@ -46,13 +45,15 @@ with open(r'E:/test.txt', 'w', encoding='utf-8') as f:
 # 操作系统往往不会立刻把数据写入磁盘，而是放到内存缓存起来，空闲的时候再慢慢写入。
 # 只有调用close()方法时，操作系统才保证把没有写入的数据全部写入磁盘。
 # 忘记调用close()的后果是数据可能只写了一部分到磁盘，剩下的丢失了。所以，还是用with语句来得保险
+import datetime
 
 with open(r'E:/test.txt', 'a+', encoding='utf-8') as f:
-    f.write('write test...\n')
+    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    f.write(time_now + ' write test...\n')
+    # 此时指针被移动到文件末尾,所有直接读取会是空白,需要把指针移回文件头
+    f.seek(0, 0)        # 0-文件头;1-当前位置;2-末尾; 1,2必须在二进制模式下使用
     print(f.read())
-
-
-
+    
 '''
 open() 的所有模式
 r	以只读方式打开文件。文件的指针将会放在文件的开头。这是默认模式。
